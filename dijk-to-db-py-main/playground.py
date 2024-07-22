dfe31fe81bf5a32aac1eaa034ca4e7146b9d165e
Uzum Stanley <70582317+uzumstanley@users.noreply.github.com>
2024-07-22 22:09:40 +0100

    Add files via upload

---
+++ dijk-to-db-py-main/playground.py
@@ -0,0 +1,8 @@
+# import geopandas as gdp
+# from pprint import pprint
+#
+# buildings = gdp.read_file('/home/denis/Documents/places.shp')
+#
+# buildings.to_csv('places.csv', index=False)
+
+print("".join(list("Hello")[::-1]))
